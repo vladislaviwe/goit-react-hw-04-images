be66bc185d1dce94d76b7cad8b41e5454c3d5c80
Vladislav Klymenko <vladislaviwe@outlook.com>
2022-10-07 15:40:20 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,23 +4,24 @@ import { Overlay, ImageModal } from './ModalStyled';
 
 import PropTypes from "prop-types";
 import { useEffect } from 'react';
+import { useCallback } from 'react';
 
 const modalRoot = document.getElementById("modal-root");
 
 export default function Modal({onClose, tags, largeImageURL}) {
+
+  const closeModal = useCallback(({target, currentTarget, code}) => {
+    if (target === currentTarget || code === "Escape") {
+        onClose();
+    }
+  }, [onClose])
 
   useEffect(() => {
     document.addEventListener("keydown", closeModal);
     return () => {
       document.removeEventListener("keydown", closeModal)
     }
-  }, [])
-
-  const closeModal = ({target, currentTarget, code}) => {
-    if (target === currentTarget || code === "Escape") {
-        onClose();
-    }
-  }
+  }, [closeModal])
 
   return (
     createPortal(
